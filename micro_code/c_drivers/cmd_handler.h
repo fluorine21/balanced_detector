d7d07e1708925ed5494b442532dcc8892df7d8aa
fluorine21 <james.an.williams@gmail.com>
2021-06-29 16:25:58 -0700

    implemented rest of functions over uart

---
+++ micro_code/c_drivers/cmd_handler.h
@@ -0,0 +1,16 @@
+/*
+ * cmd_handler.h
+ *
+ *  Created on: Jun 29, 2021
+ *      Author: James Williams
+ */
+
+#ifndef APPLICATION_USER_CORE_C_DRIVERS_CMD_HANDLER_H_
+#define APPLICATION_USER_CORE_C_DRIVERS_CMD_HANDLER_H_
+
+
+void handler_entry();
+void handle_command();
+
+
+#endif /* APPLICATION_USER_CORE_C_DRIVERS_CMD_HANDLER_H_ */
